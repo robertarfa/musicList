eef49e6f78fcd0adfe4dfaa4ff4174976b210258
robertarfa <roberta_rfa@hotmail.com>
2020-06-28 17:30:15 -0300

    new list and filter

--- src/components/GenreList.js
+++ src/components/GenreList.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import music from '../data/musicas.json'
-import MusicList from './MusicList'
+import React, { useState } from 'react';
+import music from '../data/data.json'
+// import MusicList from './MusicList'
 
 export default function GenreList() {
 
-	const { Nacionais, Internacionais, Japonesas, Italianas, Evangélicas, Infantil } = music
+	const { LISTA } = music
 
+	const [searchTerm, setSearchTerm] = useState("");
+	const [searchResults, setSearchResults] = useState([]);
+
+	const handleChange = event => {
+		setSearchTerm(event.target.value);
+	};
+
+	const handleFilter = () => {
+		const filterMusic = LISTA.filter((music) => {
+			const { TITULO, CANTOR } = music
+			return (
+				(TITULO.toLowerCase().includes(searchTerm))
+				|| (TITULO.includes(searchTerm))
+				|| (CANTOR.toLowerCase().includes(searchTerm))
+				|| (CANTOR.includes(searchTerm))
+			)
+		});
+		setSearchResults(filterMusic);
+	};
+
+	const filteredMusic = searchResults.map((musica, indice) => {
+		const { NUMERO, CANTOR, TITULO, INICIO } = musica
+		return (
+			<div key={indice} className="container">
+
+				<p >
+					Código: {NUMERO}
+				</p>
+				<p>
+					Cantor: {CANTOR}
+				</p>
+				<p>
+					Título: {TITULO}
+				</p>
+				<p>
+					Início da música: {INICIO}
+				</p>
+				{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+				<p><a href="#">Voltar para o início da lista</a></p>
+			</div>
+		)
+	})
 
 	return (
-		<div>
+		<>
+			<div className="input">
+				<input type="text" onChange={handleChange} value={searchTerm} />
+				<input type="button" onClick={handleFilter} value="Pesquisar" />
+			</div>
+			<div>
+				{filteredMusic.length === 0 ? LISTA.map((music, indice) => {
+					const { NUMERO, CANTOR, TITULO, INICIO } = music
+					return (
 
-			<h1 className="title">Lista de Músicas Karaoke</h1>
-
-			<MusicList musicGenre={Nacionais} title={"Nacionais"} />
-			<MusicList musicGenre={Internacionais} title={"Internacionais"} />
-			<MusicList musicGenre={Japonesas} title={"Japonesas"} />
-			<MusicList musicGenre={Italianas} title={"Italianas"} />
-			<MusicList musicGenre={Evangélicas} title={"Evangélicas"} />
-			<MusicList musicGenre={Infantil} title={"Infantil"} />
-
-		</div>
-	);
+						<div key={indice} className="container">
+							<p >
+								Código: {NUMERO}
+							</p>
+							<p>
+								Cantor: {CANTOR}
+							</p>
+							<p>
+								Título: {TITULO}
+							</p>
+							<p>
+								Início da música: {INICIO}
+							</p>
+							{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+							<p><a href="#">Voltar para o início da lista</a></p>
+						</div>
+					)
+				})
+					: filteredMusic}
+			</div>
+		</>
+	)
 }
-
-
