c30db75063affd0b061efc32d6e7363c88f4dd6c
Roberta Aguilar <roberta.aguilar@kobel.com.br>
2021-06-06 14:28:46 -0300

    pesquisar por letra

--- src/components/GenreList.js
+++ src/components/GenreList.js
@@ -7,7 +7,9 @@ import Loader from 'react-loader-spinner';
 export default function GenreList() {
     const { LISTA } = music;
 
-    const listAll = LISTA.map(({ NUMERO, CANTOR, TITULO, INICIO }) => {
+    const listAll = LISTA.sort(function (a, b) {
+        return a.CANTOR < b.CANTOR ? -1 : a.CANTOR > b.CANTOR ? 1 : 0;
+    }).map(({ NUMERO, CANTOR, TITULO, INICIO }) => {
         return {
             NUMERO,
             CANTOR,
@@ -23,9 +25,11 @@ export default function GenreList() {
     const [loading, setLoading] = useState(false);
     const [allMusic, setAllMusic] = useState(listAll);
     const [filteredList, setFilteredList] = useState('');
+    // const [firstLetter, setFirstLetter] = useState();
     const [activeCheckbox, setActiveCheckbox] = useState({
         titulo: false,
         cantor: true,
+        firstLetter: false,
     });
 
     const handleSubmit = (event) => {
@@ -98,25 +102,125 @@ export default function GenreList() {
         );
     }
 
+    const fcArrayOfLetters = () => {
+        const fntAllClick = () => {
+            setLoading(true);
+            setFilteredList('');
+            setAllMusic(listAll);
+            setActiveCheckbox((prev) => ({
+                ...prev,
+                cantor: true,
+                firstLetter: false,
+            }));
+            fntSetLoadFalse();
+        };
+        const letters = [
+            'a',
+            'b',
+            'c',
+            'd',
+            'e',
+            'f',
+            'g',
+            'h',
+            'i',
+            'j',
+            'k',
+            'l',
+            'm',
+            'n',
+            'o',
+            'p',
+            'q',
+            'r',
+            's',
+            'u',
+            'v',
+            'w',
+            'x',
+            'y',
+            'z',
+        ];
+
+        return (
+            <>
+                <div
+                    style={{
+                        margin: '10px',
+                        display: 'grid',
+                        gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr',
+                    }}
+                >
+                    {letters &&
+                        letters.map((a, index) => (
+                            <button
+                                key={a}
+                                style={{
+                                    margin: '10px',
+                                    padding: '5px 10px',
+                                    boxShadow: '3px 3px 5px purple',
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                }}
+                                onClick={() => fntFindFirstLetter(letters[index])}
+                            >
+                                <a href='#' key={a}>
+                                    {a?.toUpperCase()}
+                                </a>
+                            </button>
+                        ))}
+                </div>
+                <button
+                    style={{
+                        margin: '10px',
+                        padding: '5px 10px',
+                        boxShadow: '3px 3px 5px purple',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        width: '95%',
+                    }}
+                    onClick={() => fntAllClick()}
+                >
+                    TODOS OS CANTORES
+                </button>
+            </>
+        );
+    };
+
+    const fntFindFirstLetter = (letter) => {
+        // setFirstLetter(letter);
+
+        // setAllMusic(listAll.filter((a) => a.CantorNoAccentLower.charAt(0) === letter));
+
+        setLoading(true);
+
+        setAllMusic(listAll.filter((a) => a.CantorNoAccentLower.charAt(0) === letter));
+        fntSetLoadFalse();
+    };
+
     // console.log('logs allMusic', { filteredList, allMusic });
 
     return (
         <>
             <div className='input'>
                 <form onSubmit={handleSubmit} id='form'>
-                    <div>
-                        <label>
-                            {activeCheckbox.cantor
-                                ? 'Pesquisa por cantor:'
-                                : 'Pesquisa por titulo:'}
-                        </label>
-                        <input
-                            type='text'
-                            id='textSubmit'
-                            onChange={handleChange}
-                            value={filteredList?.toUpperCase()}
-                        />
-                    </div>
+                    {!activeCheckbox.firstLetter && (
+                        <div>
+                            <label>
+                                {activeCheckbox.cantor
+                                    ? 'Pesquisa por cantor:'
+                                    : 'Pesquisa por titulo:'}
+                            </label>
+                            <input
+                                type='text'
+                                id='textSubmit'
+                                onChange={handleChange}
+                                value={filteredList?.toUpperCase()}
+                            />
+                        </div>
+                    )}
+                    <div>{activeCheckbox.firstLetter && fcArrayOfLetters()}</div>
+
                     <div style={{ padding: '10px', margin: '10px', textAlign: 'center' }}>
                         <div style={{ padding: '10px', margin: '10px' }}>
                             <input
@@ -125,6 +229,7 @@ export default function GenreList() {
                                     setActiveCheckbox({
                                         cantor: true,
                                         titulo: false,
+                                        firstLetter: false,
                                     });
                                     // setFilteredList('');
                                 }}
@@ -141,6 +246,7 @@ export default function GenreList() {
                                     setActiveCheckbox({
                                         cantor: false,
                                         titulo: true,
+                                        firstLetter: false,
                                     });
                                     // setFilteredList('');
                                 }}
@@ -148,6 +254,23 @@ export default function GenreList() {
                                 checked={activeCheckbox.titulo}
                             />
                             <label>Pesquisar por titulo</label>
+                        </div>
+
+                        <div style={{ padding: '10px', margin: '10px' }}>
+                            <input
+                                type='radio'
+                                onChange={() => {
+                                    setActiveCheckbox({
+                                        cantor: false,
+                                        titulo: false,
+                                        firstLetter: true,
+                                    });
+                                    // setFilteredList('');
+                                }}
+                                value={activeCheckbox.firstLetter}
+                                checked={activeCheckbox.firstLetter}
+                            />
+                            <label>Pesquisar pela primeira letra do nome do cantor</label>
                         </div>
                     </div>
                     <div className='button'>
